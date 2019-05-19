5db222e74d345626af7d676487b73db710d31c67
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2019-05-19 01:57:30 -0300

    Criado a classe para os currículos

---
+++ models/curriculo.py
@@ -0,0 +1,10 @@
+class Curriculo:
+    def __init__(self, nome_completo, idade, email, endereco, objetivo_profissional, experiencias_anteriores,
+                 cursos_complementares):
+        self.nome_completo = nome_completo
+        self.idade = idade
+        self.email = email
+        self.endereco = endereco
+        self.objetivo_profissional = objetivo_profissional
+        self.experiencias_anteriores = experiencias_anteriores
+        self.cursos_complementares = cursos_complementares
