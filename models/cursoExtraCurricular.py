536c7e8ecf5c2c6b8b3df56da4865a592f1eab36
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2019-05-19 12:50:51 -0300

    Adicionado o campo de descrição do curso extracurricular

--- models/cursoExtraCurricular.py
+++ models/cursoExtraCurricular.py
@@ -1,7 +1,8 @@
 class CursoExtraCurricular:
-    def __init__(self, nome, instituicao, duracao, cursando, data_inicial):
+    def __init__(self, nome, instituicao, duracao, cursando, data_inicial, descricao=''):
         self.nome = nome
         self.instituicao = instituicao
         self.duracao = duracao
         self.cursando = cursando
         self.data_inicial = data_inicial
+        self.descricao = descricao
