0af8f054b58bda93dc5cea6361af3bd75ddbaf76
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2019-05-19 21:38:57 -0300

    Adicionado o atributo para pontuação por idioma

--- models/pontuacao.py
+++ models/pontuacao.py
@@ -1,6 +1,6 @@
 class Pontuacao:
     def __init__(self, pontuacao_minima, candidato_id, pontuacao_objetivo, pontuacao_experiencias_anteriores,
-                 pontuacao_cursos_extracurriculares, pontuacao_endereco, pontuacao_id=None):
+                 pontuacao_cursos_extracurriculares, pontuacao_endereco, pontuacao_idioma, pontuacao_id=None):
         self.pontuacao_id = pontuacao_id
         self.candidato_id = candidato_id
         self.pontuacao_minima = pontuacao_minima
@@ -8,6 +8,7 @@ class Pontuacao:
         self.pontuacao_experiencias_anteriores = pontuacao_experiencias_anteriores
         self.pontuacao_cursos_extracurriculares = pontuacao_cursos_extracurriculares
         self.pontuacao_endereco = pontuacao_endereco
+        self.pontuacao_idioma = pontuacao_idioma
 
     @property
     def pontuacao_alcancada(self):
