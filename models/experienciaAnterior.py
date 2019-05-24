3e1f780a6ddf6a4667e67c0b020d56c97a3eb8a5
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2019-05-23 23:55:23 -0300

    Definição da classe e atributos das experiências anteriores

---
+++ models/experienciaAnterior.py
@@ -0,0 +1,9 @@
+class ExperienciaAnterior:
+    def __init__(self, cargo, nome_empresa, emprego_atual, data_entrada, data_saida, principais_atividades, salario):
+        self.cargo = cargo
+        self.nome_empresa = nome_empresa
+        self.emprego_atual = emprego_atual
+        self.data_entrada = data_entrada
+        self.data_saida = data_saida
+        self.principais_atividades = principais_atividades
+        self.salario = salario
