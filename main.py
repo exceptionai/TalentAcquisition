2d4fd7961a5f054c56e3b833bf8d5c9a37f04737
Alisson Chabaribery <alissonchabaribery6@gmail.com>
2019-09-10 19:59:56 -0300

    responsa do renan

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
 from flask import Flask
-from connections.connectionFactory import ConnectionFactory
-from DAO.proficienciaDAO import ProficienciaDAO
-from models.proeficiencia import Proeficiencia
- 
 app = Flask(__name__)
 
-proeficiencia = Proeficiencia('baixo','alto','baixo')
-
-dao = ProficienciaDAO(proeficiencia)
-
-dao.insere()
-# ConnectionFactory.execute('SELECT * FROM proficiencia')
-# resultado = ConnectionFactory.fetchall()
-
-# print(resultado)
 
 
 from routes import *
