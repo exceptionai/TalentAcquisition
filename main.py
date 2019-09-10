7cc26d3b834e701a3c03448a928ff74256bf15be
Alisson Chabaribery <alissonchabaribery6@gmail.com>
2019-09-10 20:52:09 -0300

    ajustando bugs

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 from flask import Flask
-<<<<<<< HEAD
- 
-app = Flask(__name__)
-
-# ConnectionFactory.execute('SELECT * FROM proficiencia')
-# resultado = ConnectionFactory.fetchall()
-
-# print(resultado)
-=======
 from livereload import Server, shell
->>>>>>> 8b8b4193b55a44048df2d041b8347591c085dc43
 
 app = Flask(__name__)
 
